4986a480ad6f9caf0d7d19088a1c31e6b0ab5133
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2022-04-06 14:05:07 +0530

    2-frontend-setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("Home");
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server is listening at port ${PORT}...`);
   console.log(`[app]: http://localhost:${PORT}`);
