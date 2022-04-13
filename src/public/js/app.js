04be49a02fc7e1c8378020d8c48256b109c655a9
VaibhavSethia07 <E19CSE321@bennett.edu.in>
2022-04-13 15:42:24 +0530

    22-Handle button clicks and show cameras list

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,10 +2,29 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 let stream = null;
 let mute = false;
 let cameraOff = false;
+
+const getCameras = async () => {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    console.log(devices);
+    console.log(cameras);
+
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.label = camera.label;
+      option.value = camera.deviceId;
+      camerasSelect.appendChild(option);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const getMedia = async () => {
   try {
@@ -14,24 +33,32 @@ const getMedia = async () => {
       video: true,
     });
     myFace.srcObject = stream;
+    console.log(stream);
+    await getCameras();
   } catch (err) {}
 };
 
 muteBtn.addEventListener("click", () => {
+  const tracks = stream.getAudioTracks();
+  console.log(tracks);
   if (mute) {
     muteBtn.innerText = "Mute";
   } else {
     muteBtn.innerText = "Unmute";
   }
+  tracks.forEach((track) => (track.enabled = !track.enabled));
   mute = !mute;
 });
 
 cameraBtn.addEventListener("click", () => {
+  const tracks = stream.getVideoTracks();
+  console.log(tracks);
   if (cameraOff) {
     cameraBtn.innerText = "Turn Camera Off";
   } else {
     cameraBtn.innerText = "Turn Camera On";
   }
+  tracks.forEach((track) => (track.enabled = !track.enabled));
   cameraOff = !cameraOff;
 });
 
